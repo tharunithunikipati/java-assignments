1457bbc20a36c5c0e5fc5881f0461c93ff11852a
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-08-24 22:20:25 +0530

    Update Areacircle.java

--- Assignment/src/Basic/Areacircle.java
+++ Assignment/src/Basic/Areacircle.java
@@ -1,10 +1,8 @@
-//package Basic;
-//
-//public class Areacircle {
-//public static void main(String args[]) {
-//	int r=5;
-//	double area=3.14*r*r;
-//	System.out.println("area is:"+area);
-//	
-//}
-//}
+package Basic;
+public class Areacircle {
+public static void main(String args[]) {
+	int r=5;
+	double area=3.14*r*r;
+	System.out.println("area is:"+area);	
+}
+}
