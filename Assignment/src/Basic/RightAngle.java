4c609239c40605949a2f9725cd68c640d3f319dc
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-09-27 10:16:49 +0530

    Update RightAngle.java

--- Assignment/src/Basic/RightAngle.java
+++ Assignment/src/Basic/RightAngle.java
@@ -4,8 +4,7 @@ public class RightAngle {
 	public static void main(String args[]) {
 		for (int i=1;i<=4;i++) {
 			for(int j=1;j<=i;j++) {
-				System.out.print("*");
-			
+				System.out.print("*");	
 			}
 			System.out.println();
 		}
