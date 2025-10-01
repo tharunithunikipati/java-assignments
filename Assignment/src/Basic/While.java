8723d79f4fd148e0a178e876c968967007963a36
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-10-01 21:42:43 +0530

    Update While.java

--- Assignment/src/Basic/While.java
+++ Assignment/src/Basic/While.java
@@ -1,17 +1,12 @@
 package Basic;
-
 public class While {
-
 	public static void main(String[] args) {
-		
 				int count=0;
 				for(int i=1;i<=10;i++) {
 					if(i%2!=0)
-					{
-						
+					{	
 						System.out.println(i);
 						count++;
-						
 					}if(count==3) {
 						break;
 						
