b77d1fbbd385dae719610c6b8d804670ec0f7654
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-10-14 15:15:56 +0530

    Update Average.java

--- Assignment/src/Basic/Average.java
+++ Assignment/src/Basic/Average.java
@@ -3,7 +3,6 @@ public class Average {
 	public static void main(String args[]) {
 		int a=1,b=3,c=7,d=8,e=9;
 		 double average=(a+b+c+d+e)/5;
-		 System.out.println("Average of two numbers:"+average);
-		
+		 System.out.println("Average of two numbers:"+average);	
 	}
 }
