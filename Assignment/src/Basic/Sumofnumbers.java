5b185e9cd555e0857742ecb23e1f0e5c25dba4d0
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-10-24 10:45:44 +0530

    Update Sumofnumbers.java

--- Assignment/src/Basic/Sumofnumbers.java
+++ Assignment/src/Basic/Sumofnumbers.java
@@ -1,13 +1,11 @@
 package Basic;
 import java.util.*;
 public class Sumofnumbers {
-	
 		public static void main(String args[]) {
 			int num=10,sum=20;
 			for (int i=10;i<=10;i++){
 				System.out.println("Sum="+sum);
 			}
 		}
-
 	}
 
