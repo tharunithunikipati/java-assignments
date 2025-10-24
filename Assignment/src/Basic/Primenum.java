38abc9c5742254e338382957696805358310b8b7
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-10-24 10:44:02 +0530

    Update Primenum.java

--- Assignment/src/Basic/Primenum.java
+++ Assignment/src/Basic/Primenum.java
@@ -1,18 +1,15 @@
 package Basic;
 import java.util.*;
 public class Primenum {
-
 	public static void main(String[] args) {
 		int n=11;
 		boolean isPrime=true;
 		for(int i=2;i<=n/2;i++) {
 			if(n%i==0) {
 				isPrime=false;
-				break;
-				
+				break;	
 			}
 		}
 		System.out.println(n+(isPrime?"is Prime":"is not Prime"));
 	}
-
 }
